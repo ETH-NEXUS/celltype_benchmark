68b269627e7a467c00778440096d2d56c363e6f0
Marcus Lindberg <mlindberg@ethsec-login-02.leonhard.ethz.ch>
2020-08-13 06:47:41 +0200

    [FIX] Fixed data conversion for Garnett with proper accessors/slots for data.

--- scripts/convert_sce_to_cds.R
+++ scripts/convert_sce_to_cds.R
@@ -1,13 +1,15 @@
 # Script for converting SCE object (saved as RDS) into a Monocle3-compliant Cell_Data_Set (CDS) object as input for the Garnett cell typer.
 # Requires conda environment installed via /doc/conda_envs/garnett.yaml
 
+library(optparse)
 library(monocle3)
 library(org.Hs.eg.db)
 
 option_list = list(
   make_option("--sce_in", type = "character", help = "Path to RDS file containing SCE data object."),
-  make_option("--cds_out", type = "character", help = "Path to output file of final (converted) CDS object."),
-  make_option("--sample_name", type = "character", help = "Sample name of input SCE data.")
+  make_option("--output_dir", type = "character", help = "Path to output file of final (converted) CDS object."),
+  make_option("--sample_name", type = "character", help = "Sample name of input SCE data."),
+  make_option("--features_file", type = "character", help = "Path to CellRanger features.tsv.gz file used for cross-referencing gene IDs with gene symbols.")
 )
 
 opt_parser = OptionParser(option_list = option_list)
@@ -28,13 +30,13 @@ data <- monocle3::new_cell_data_set(expression_data=data@assays$data$counts)
 
 # Add data/annotations manually to converted CDS object
 fData(data)$gene_id <- gene_ids
-fData(data)$umap_cl <- umap_cl
+pData(data)$umap_cl <- umap_cl #is pData accurate?
 fData(data)$ground_truth_major <- ground_truth_labels[1]
 fData(data)$ground_truth_minor <- ground_truth_labels[2]
 reducedDims(data)$UMAP <- umap_hvg
 
 # Add shortened gene names to CDS
-features <- read.table("/cluster/project/nexus/benchmarking/celltyping/test_pilot/cellranger_run/zheng_sorted_merged/outs/filtered_feature_bc_matrix/features.tsv.gz") #TO-DO: add universal features files in a /data/ dir & point to it using config file.
+features <- read.table(opt$features_file)
 features <- features[1:2]
 names(features) <- c("gene_id","gene_short_name")
 match_index <- match(row.names(data), features$gene_id)
