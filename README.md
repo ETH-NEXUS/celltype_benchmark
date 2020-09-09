8b8268dc5749b29069f2c89813e76a597b54cea9
Marcus Lindberg <mlindberg@ethsec-login-03.leonhard.ethz.ch>
2020-09-09 10:53:28 +0200

    [INTERNAL] Updated readme w/ system requirements (on Leomed). Updated SCE extension script (also adding original cell types).

--- README.md
+++ README.md
@@ -5,6 +5,22 @@ Pipeline to run individual cell typing methods and compare the accuracies of eac
 Make sure `miniconda` is installed as `conda` environments will be used for some of the cell typing methods.
 All R packages and Python modules will be installed via conda.
 
-## Usage:
+On Leomed:
+When running each pipeline with snakemake, make sure to use the '--use-conda' argument to install and use the correct conda environment. The list of packages that will be installed in the conda environment can be found in the YML file in the `doc` folder.
 
+## Input data format:
+The data to be used as input for the benchmark needs to be an SCE file containing pre-processed counts data. Additionally, a tab-separated values (tsv) file containing a column with barcodes and a column with cell type ground-truth labels needs to be provided. Use the `add_sce_groundtruth.R` script to annotate the SCE data file with the ground-truth labels. This is the complete file to be used as input for the benchmark pipelines.
+
+A simple sample map is used:
+```
+1	sample_name	N	1
+```
+Currently, only the sample name needs to be substituted.
+
+## Usage:
+The benchmark consists of two pipelines: a training pipeline and a testing pipeline.
+
+For the training pipeline, the provided `training_master.snake` snake file should be used.
+
+Once a training run has been successfully completed, you will have the input files needed to run the latter testing pipeline. For the testing pipeline, the `testing_master.snake` snake file should be used.
 
--- scripts/add_sce_groundtruth.R
+++ scripts/add_sce_groundtruth.R
@@ -4,29 +4,45 @@ library(SingleCellExperiment)
 option_list = list(
   make_option("--sce_in", type = "character", help = "Input path to processed SCE object (.RDS format)."),
   make_option("--labels_in", type = "character", help = "Path to ground-truth label file. Required structure: column 1 = barcodes (full length), column 2 = major cell type, column 3 = minor cell type."),
+  make_option("--matched_labels", type = "character", help = "Path to dictionary file mapping sample-specific cell type labels to universal labels for cross-sample comparisons."),
   make_option("--sce_out", type = "character", help = "Output path of annotated SCE object (.RDS format).")
 )
 
 opt_parser = OptionParser(option_list = option_list)
 opt = parse_args(opt_parser)
 
-add_sce_groundtruth <- function(rds_path, labels_path, output_path){
+add_sce_groundtruth <- function(rds_path, labels_path, matched_labels_dictionary = NULL, output_path){
     sce <- readRDS(rds_path)
+    sce@metadata$true_labels <- runif(ncol(sce))
     sce@metadata$ground_truth_major <- runif(ncol(sce))
     sce@metadata$ground_truth_minor <- runif(ncol(sce))
 
-    labels <- read.table(labels_path, header = TRUE, sep ='\t')
-    major_label <- labels$major_label
-    minor_label <- labels$minor_label
-    names(major_label) <- labels$barcode
-    names(minor_label) <- labels$barcode
+    labels <- read.table(labels_path, header = FALSE, sep ='\t', skip = 1)
+    true_major_label <- labels[,2]
+    print(head(true_major_label))
+    matched_major_label <- labels[,3]
+    matched_minor_label <- labels[,4]
+    names(true_major_label) <- labels[,2]
+    names(matched_major_label) <- labels[,3]
+    names(matched_minor_label) <- labels[,4]
     
-    match_index <- match(sce$barcodes, substr(names(major_label),1,16))
-
-    sce@metadata$ground_truth_major <- as.character(major_label[match_index])
-    sce@metadata$ground_truth_minor <- as.character(minor_label[match_index])
+    #match_index <- match(sce$barcodes, substr(names(matched_major_label),1,16))
+    match_index <- match(sce$barcodes, labels[,1])
+    if(NA %in% match_index){
+	unmatched_index <- which(is.na(match_index))
+        print(unmatched_index)
+	missing_index <- match(sce$barcodes[unmatched_index], substr(labels[,1],1,16))
+	print(sce$barcodes[unmatched_index]);print(substr(labels[,1][unmatched_index],1,16));
+	match_index[unmatched_index] <- missing_index
+    }
+    if(NA %in% match_index){
+        print(paste("Warning:",length(which(is.na(match_index))),"cell barcodes not matched to cell types."))
+    }
+    sce@metadata$ground_truth_major <- as.character(matched_major_label[match_index])
+    sce@metadata$ground_truth_minor <- as.character(matched_minor_label[match_index])
+    sce@metadata$true_labels <- as.character(true_major_label[match_index])
     
     saveRDS(sce, output_path)
     }
 
-add_sce_groundtruth(opt$sce_in, opt$labels_in, opt$sce_out)
+add_sce_groundtruth(opt$sce_in, opt$labels_in, opt$matched_labels, opt$sce_out)
