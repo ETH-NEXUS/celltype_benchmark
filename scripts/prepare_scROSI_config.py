3eab408e0fa15c949ae811daf9a2c79e37b6c544
Marcus Lindberg <mlindberg@ethsec-login-03.leonhard.ethz.ch>
2020-10-06 13:29:59 +0200

    [FIX] Adapted prepare_scROSI_config.py to match testing pipeline call/args.

--- scripts/prepare_scROSI_config.py
+++ scripts/prepare_scROSI_config.py
@@ -2,23 +2,23 @@ import argparse
 import pandas as pd
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--gmx_in', metavar='type=string',
+parser.add_argument('--gmx', metavar='type=string',
                     help='Input path to GMX marker file.')
-parser.add_argument('--config', help='Input path to ground-truth config file.')
-parser.add_argument('--gmx_out', help='Output path to GMX file to which matching celltype_config will be written.')
+parser.add_argument('--config_groundtruth', help='Input path to ground-truth config file.')
+parser.add_argument('--outfile', help='Output path to tsv file to which matching celltype_config will be written.')
 opt = parser.parse_args()
 
 # read in header of GMX file
 # read in config file
 # compare which of the ground truth cell types are present in the header you just extracted
 
-gmx_file = open(opt.gmx_in).read()
+gmx_file = open(opt.gmx).read()
 gmx_header = gmx_file.split('\n', 1)[0].split("\t")
 
 config_dictionary = {}
 config_subtypes = []
 config_major_types = []
-with open(opt.config) as config_file:
+with open(opt.config_groundtruth) as config_file:
     next(config_file)
     for line in config_file:
        (major_type, sub_type) = line.split()
@@ -42,9 +42,9 @@ print("Matching cell types: ", matched_types)
 print("Cell types in config file but not in GMX: ", [cell_type for cell_type in all_types if not cell_type in gmx_header])
 print("Cell types in GMX but not in config file: ", [cell_type for cell_type in gmx_header if not cell_type in all_types])
 
-gmx_table = pd.read_table(opt.gmx_in, skip_blank_lines = False, usecols = matched_types) # need to make sure cell types in config file are in GMX...
+gmx_table = pd.read_table(opt.gmx, skip_blank_lines = False, usecols = matched_types) # need to make sure cell types in config file are in GMX...
 gmx_table.fillna('', inplace=True)
 print(gmx_table)
-print("Saving modified GMX file...")
-gmx_table.to_csv(opt.gmx_out, sep="\t", index = False)
+print("Saving modified config file to ", opt.outfile, "...")
+gmx_table.to_csv(opt.outfile, sep="\t", index = False)
 print("Done.")
